dc26cf338d8cb21098f3db5b5e7381be248707ef
YiJinKim <yijin3018@gmail.com>
2017-12-05 22:19:22 +0900

    Main.java

--- swProject/src/swProject/Main.java
+++ swProject/src/swProject/Main.java
@@ -1,15 +1,20 @@
 package swProject;
 
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 	
 	static int input;
 	static Scanner sc = new Scanner(System.in);
+	static Memo memo;
 	static Calculator calculator;
+	static HouseHold household;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
+		memo = new Memo();
 		calculator = new Calculator();
+		household = new HouseHold();
 		
 		System.out.println("1.메모");
 		System.out.println("2.계산기");
@@ -21,12 +26,12 @@ public class Main {
 		
 		switch(input) {
 		case 1:
-			
+			memo.memoMenu();
 		case 2:
 			calculator.calculatorMenu();
 			break;
 		case 3:
-			
+			household.houseHoldMenu();
 			
 		}
 
