9c5e9991eeb945f127f655ca8f6f335ad8f83343
unknown <djwldls0405@gmail.com>
2017-12-23 02:00:59 +0900

    add while statement

--- swProject/src/swProject/Main.java
+++ swProject/src/swProject/Main.java
@@ -16,26 +16,29 @@ public class Main {
 		calculator = new Calculator();
 		household = new HouseHold();
 		
-		System.out.println("1.메모");
-		System.out.println("2.계산기");
-		System.out.println("3.가계부");
-		System.out.println("4.프로그램 종료");
-
-		input = sc.nextInt();
-		//sc.nextLine();
+		boolean flag = true;
 		
-		switch(input) {
-		case 1:
-			memo.memoMenu();
-			break;
-		case 2:
-			calculator.calculatorMenu();
-			break;
-		case 3:
-			household.houseHoldMenu();
-			break;			
-		}
+		while(flag){
+			System.out.println("1.메모");
+			System.out.println("2.계산기");
+			System.out.println("3.가계부");
+			System.out.println("4.프로그램 종료");
 
+			input = sc.nextInt();
+			
+			if(input == 1){
+				memo.memoMenu();
+				
+			}else if(input == 2){
+				calculator.calculatorMenu();
+				
+			}else if(input ==3){
+				household.houseHoldMenu();
+				
+			}else if(input == 4){
+				flag = false;
+			}
+		}
 	}
 
 }
